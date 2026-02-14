95f5a4f9c008708f59062df1c32be6dcc8160acb
Gavin Morrow <gavinfmorrow@gmail.com>
2026-02-14 12:16:02 +0100

    Allow building only specific client start.sh

--- start.sh
+++ start.sh
@@ -3,10 +3,14 @@
 if ! [ "$1" = "--no-client" ]; then
   pushd ./client
 
-  # TODO: automate this somehow?
-  gleam run -m lustre/dev build subway_gleam/client/stops
-  gleam run -m lustre/dev build subway_gleam/client/stop
-  gleam run -m lustre/dev build subway_gleam/client/train
+  if [ "$1" = "--client" ]; then
+    gleam run -m lustre/dev build subway_gleam/client/$2
+  else
+    # TODO: automate this somehow?
+    gleam run -m lustre/dev build subway_gleam/client/stops
+    gleam run -m lustre/dev build subway_gleam/client/stop
+    gleam run -m lustre/dev build subway_gleam/client/train
+  fi
 
   popd
 fi
